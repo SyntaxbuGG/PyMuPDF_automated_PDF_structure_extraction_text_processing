a7cc9b7f4118edda04e930f16bd8970ca9667eb3
SyntaxBugg <zarbotbekovabbosbek@gmail.com>
2024-11-04 15:03:23 +0500

    modifu code 1

--- pymupdf_test_final.py
+++ pymupdf_test_final.py
@@ -7,8 +7,12 @@ pdf_file = fitz.open("filefortest.pdf")
 
 toc = pdf_file.get_toc()
 
+
+chapter_pattern = re.compile(r"^[А-Яа-я]+(\s+[А-Яа-я]+(,\s*)?){0,20}$")
 section_pattern = re.compile(r'^\d+\.\d(\.)?+\s')
 subsection_pattern = re.compile(r'^\d+\.\d+\.\d+\s')
+text_pattern_stop = re.compile(
+    r'\b\d+\.\d+(\.\d+)?\.?\s+([А-Я][а-я]+|[А-Я]+)')
 
 
 def extract_text_from_pages(title_text: str, start_page: int) -> str:
@@ -16,14 +20,12 @@ def extract_text_from_pages(title_text: str, start_page: int) -> str:
     finally_text = ""
     text_without_num = ''
 
-    section_text_pattern = re.compile(
-        r'\b\d+\.\d+(\.\d+)?\.?\s+([А-Я][а-я]+|[А-Я]+)')
 
-    # Это чтобы несколько пробелов или какихто , невидимох символов заменить на регэксп пробел
-    pattern_string = title_text.replace(" ", r"\s*")
+    # Это чтобы несколько пробелов или какихто невидимох символов заменить на регэксп пробел
+    pattern_title = title_text.replace(" ", r"\s*")
 
     # Находит по заголовок игнорируя загланые и строчные буквы
-    pattern = re.compile(rf'{pattern_string}(.*)', re.DOTALL | re.IGNORECASE)
+    pattern = re.compile(rf'{pattern_title}(.*)', re.DOTALL | re.IGNORECASE)
 
     try:
         total_pages = pdf_file.page_count
@@ -31,13 +33,10 @@ def extract_text_from_pages(title_text: str, start_page: int) -> str:
             page = pdf_file.load_page(start_page - 1)
             page_text = page.get_text("text")
             match = pattern.search(page_text)
-            if match:
-                text_without_num = match.group(1).lstrip()
 
-            else:
-                text_without_num = page_text
+            text_without_num = match.group(1).lstrip() if match else page_text
 
-            match_text = section_text_pattern.search(text_without_num)
+            match_text = text_pattern_stop.search(text_without_num)
 
             if match_text:
                 match_section = match_text.start()
@@ -62,26 +61,24 @@ def correct_level(level: int, title: str) -> int:
 
 def extract_structure(file: list[list]) -> dict[str, dict]:
     structure = {}
-    chapter = 0
+    current_chapter = 0
 
     for line in file[1:]:
         level, title, page = line
         level = correct_level(level, title)
         without_numbers_title = ' '.join(title.split()[1:])
-        if level == 1:
-            if 'Глава' in title:
-                chapter += 1
-                structure[str(chapter)] = {}
-                current_chapter = str(chapter)
-            else:
-                chapter_section = extract_text_from_pages(title, page)
-                if chapter_section:
-                    structure[current_chapter] = {
+        text_from_page = extract_text_from_pages(without_numbers_title, page)
+
+        if level == 1 and chapter_pattern.match(title):
+            current_chapter += 1
+            chapter_section = extract_text_from_pages(title, page)
+            if chapter_section:
+                structure[current_chapter] = {
                         'title': title,
                         'text': chapter_section,
                         'sections': {},
                     }
-                else:
+            else:
                     structure[current_chapter] = {
                         'title': title,
                         'sections': {}
@@ -89,12 +86,11 @@ def extract_structure(file: list[list]) -> dict[str, dict]:
 
         elif level == 2 and section_pattern.match(title):
             section_number = title.split()[0]
-            text_section = extract_text_from_pages(without_numbers_title, page)
 
-            if text_section:
+            if text_from_page:
                 structure[current_chapter]['sections'][section_number] = {
                     'title': without_numbers_title,
-                    'text': text_section,
+                    'text': text_from_page,
                     'subsections': {},
                 }
             else:
@@ -107,12 +103,11 @@ def extract_structure(file: list[list]) -> dict[str, dict]:
 
         elif level == 3 and subsection_pattern.match(title):
             subsections_number = title.split()[0]
-            text_subsection = extract_text_from_pages(
-                without_numbers_title, page)
-            if text_subsection:
+
+            if text_from_page:
                 structure[current_chapter]['sections'][section_number]['subsections'][subsections_number] = {
                     'title': without_numbers_title,
-                    'text': text_subsection
+                    'text': text_from_page
                 }
             else:
                 structure[current_chapter]['sections'][section_number]['subsections'][subsections_number] = {
@@ -124,6 +119,7 @@ def extract_structure(file: list[list]) -> dict[str, dict]:
 
 extracted_structure = extract_structure(toc)
 
+print(toc)
 
 with open('structuretestfinal.json', 'w', encoding='utf-8') as json_file:
     json.dump(extracted_structure, json_file, ensure_ascii=False, indent=4)
